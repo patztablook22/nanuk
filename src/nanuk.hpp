19446f416993e6be29229787ca46a206ded6f78d
patztablook22 <patz@tuta.io>
2021-06-28 22:09:01 +0200

    more headers

---
+++ src/nanuk.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <vector>
+#include <fstream>
+#include <cmath>
+
+namespace nanuk {
+    
+    using namespace std;
+    using Scalar = double;
+    using Tensor1D = vector<Scalar>;
+    using Tensor2D = vector<Tensor1D>;
+    
+    class Neuron;
+    using Layer  = vector<Neuron>;   // dense-only layer
+    
+    Scalar (* activation_function)(Scalar) = &::tanh;
+
+
+    struct Synapse {
+        Scalar weight;
+    };
+    
+
+    class Neuron {
+        Scalar bias;
+        vector<Synapse> synapsesIn;
+        Scalar memory;
+
+        public:
+            Neuron(unsigned);
+            void feed_forward(Scalar);
+            void feed_forward(Layer&);
+            Scalar read();
+    };
+    
+
+    class Nanuk {
+        vector<Layer> layers;
+
+        public:
+            Nanuk(vector<unsigned>&);
+            void operator<<(Tensor2D&);
+            Tensor1D operator()(Tensor1D&);
+            void inspect();
+        
+        private:
+            Tensor1D output();
+            void feed_forward(Tensor1D&);
+            void propagate_back(Tensor1D&);
+            
+    };
+}
